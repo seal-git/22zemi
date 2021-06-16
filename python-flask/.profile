6ddaf75bfac9be9368c70b7dec7bc56d6532a4e8
yukiyoshianri <seal0828.4477@gmail.com>
2021-06-16 11:01:49 +0900

    update

--- python-flask/.profile
+++
@@ -1,1 +0,0 @@
-chown -R yuki-yos:ubuntu ./
--- python-flask/app/__init__.py
+++ python-flask/app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import app.config
-
+from flask_sqlalchemy import SQLAlchemy 
 
 app_ = Flask(__name__)
 app_.config.from_object(config.Config)
 CORS(app_)
+
 # アプリでDB操作を行えるように初期設定する
 db_ = SQLAlchemy(app_)
 db_.init_app(app_)
