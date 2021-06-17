fa1aa361146e4dc0312970b028eba67fef6bbb1d
milmilk <o1610158@edu.cc.uec.ac.jp>
2021-06-18 06:52:57 +0900

    Notify user if unanimous

--- python-flask/app/models.py
+++ python-flask/app/models.py
@@ -15,7 +15,7 @@ mysqlサーバーとの接続はmysql.connectorで行っているが，SQLAlchem
 
 
 # 現在アプリを使っているグループやユーザを格納する
-current_group = {} # {group_id1: {'Coordinates': (lat,lon), 'Users': { 'user_id1: {'RequestCount': 0, 'Feeling': {restaurant_id1: true, ... }}, ... }}, ... }
+current_group = {} # {group_id1: {'Coordinates': (lat,lon), 'Users': { 'user_id1: {'RequestCount': 0, 'Feeling': {restaurant_id1: true, ... }}, ... }, 'Unanimous': [restaurant_id1, ... ]}, ... }
 
 
 # mysqlサーバーと接続
@@ -34,42 +34,26 @@ if conn.is_connected():
     print("db connected!")
 
 
-# Yahoo local search APIで情報を取得し、json形式で情報を返す
-# @param coordinates : (int, int) (緯度, 経度)のタプル
-# @param request_count : int 何回目のリクエストかを指定する。回数に応じて別の店舗を返す。同じ値を指定したら同じ結果が返るはず。
-def get_info_from_yahoo_local_search(coordinates, request_count):
+# Yahoo local search APIで取得した店舗情報(feature)を、クライアントに送信するjsonの形式に変換する。
+# @param local_search_query : dictionary : Yahoo local search APIに送信するクエリ
+# @return : dictionary : クライアントに送信するjsonのもと
+def get_restaurant_info_from_local_search_query(coordinates, local_search_query):
     MAX_LIST_COUNT = 10
-    RESULTS_COUNT = 25 # 一回に取得する店舗の数
-    
+
     now_time = datetime.datetime.now().hour + datetime.datetime.now().minute / 60
     lunch_or_dinner = 'lunch' if 10 <= now_time and now_time <15 else 'dinner' # 現在時刻でランチかディナーか決定する。価格表示に使用している。今のところ検索には使用していない。
 
-    #  ヤフー本社から1km以内のグルメを検索
-    local_search_query = {} # YahooローカルサーチAPIで検索するクエリ
+    # Yahoo local search APIで店舗情報を取得
     local_search_query['appid'] = os.environ['YAHOO_LOCAL_SEARCH_API_CLIENT_ID']
-        # 検索条件↓
-    local_search_query['lat'] = coordinates[0] # 緯度
-    local_search_query['lon'] = coordinates[1] # 経度
-    local_search_query['dist'] = 3 # 中心地点からの距離 # 最大20km
-    local_search_query['gc'] = '01' # グルメ
-    local_search_query['image'] = True # 画像がある店
-    local_search_query['open'] = 'now' # 現在開店している店舗
-    local_search_query['sort'] = 'hybrid' # 評価や距離などを総合してソート
-        # 表示方法↓
     local_search_query['output'] = 'json' #
     local_search_query['detail'] = 'full' #
-    local_search_query['start'] = RESULTS_COUNT * request_count #
-    local_search_query['results'] = RESULTS_COUNT #
-
-    # Yahoo local search APIで店舗情報を取得
     with urllib.request.urlopen('https://map.yahooapis.jp/search/local/V1/localSearch?' + urllib.parse.urlencode(local_search_query)) as response:
         local_search_json = json.loads(response.read())
 
     # 検索の該当が無かったとき
     if local_search_json['ResultInfo']['Count'] == 0:
         return "{}"
-    
-    # 店舗情報をjson形式で返す
+
     result_json = []
     for i,feature in enumerate(local_search_json['Feature']):
         result_json.append({})
@@ -87,6 +71,31 @@ def get_info_from_yahoo_local_search(coordinates, request_count):
         result_json[i]['Images'] = lead_image + image_n + persistency_image_n
 
     return json.dumps(result_json)
+
+
+# Yahoo local search APIで情報を取得し、json形式で情報を返す
+# @param coordinates : (int, int) : (緯度, 経度)のタプル
+# @param request_count : int : 何回目のリクエストかを指定する。回数に応じて別の店舗を返す。同じ値を指定したら同じ結果が返るはず。
+# @return : string : json形式
+def get_info_from_yahoo_local_search(coordinates, request_count):
+    RESULTS_COUNT = 25 # 一回に取得する店舗の数
+    
+    #  ヤフー本社から1km以内のグルメを検索
+    local_search_query = {} # YahooローカルサーチAPIで検索するクエリ
+        # 検索条件↓
+    local_search_query['lat'] = coordinates[0] # 緯度
+    local_search_query['lon'] = coordinates[1] # 経度
+    local_search_query['dist'] = 3 # 中心地点からの距離 # 最大20km
+    local_search_query['gc'] = '01' # グルメ
+    local_search_query['image'] = True # 画像がある店
+    local_search_query['open'] = 'now' # 現在開店している店舗
+    local_search_query['sort'] = 'hybrid' # 評価や距離などを総合してソート
+        # 表示方法↓
+    local_search_query['start'] = RESULTS_COUNT * request_count #
+    local_search_query['results'] = RESULTS_COUNT #
+
+    # Yahoo local search APIで店舗情報を取得
+    return get_restaurant_info_from_local_search_query(coordinates, local_search_query)
 
 
 @app_.after_request
@@ -123,17 +132,17 @@ def get_sample_db():
 
 @app_.route('/init')
 # 最初にリクエストするやつ
-def get_init():
+def http_init():
     user_id = request.args.get('user_id')
     group_id = request.args.get('group_id')
-    location = request.args.get('location')
+    # coordinates = request.args.get('coordinates') # 位置情報
     
     # Yahoo本社の座標を決め打ち
     lat = 35.68001 # 緯度
     lon = 139.73284 # 経度
     
     if group_id not in current_group:
-        current_group[group_id] = {'Coordinates': (lat,lon), 'Users': {}}
+        current_group[group_id] = {'Coordinates': (lat,lon), 'Users': {}, 'Unanimous': []}
     if user_id not in current_group[group_id]['Users']:
         current_group[group_id]['Users'][user_id] = {'RequestCount': 0, 'Feeling': {}}
 
@@ -143,7 +152,7 @@ def get_init():
 
 @app_.route('/more')
 # 2回目以降、さらに店舗を検索する
-def get_more():
+def http_more():
     user_id = request.args.get('user_id')
     group_id = request.args.get('group_id')
     
@@ -154,14 +163,20 @@ def get_more():
 
 @app_.route('/feeling')
 # キープ・リジェクトの結果を受け取る。メモリに格納
-def post_feeling():
+def http_feeling():
     user_id = request.args.get('user_id')
     group_id = request.args.get('group_id')
     restaurant_id = request.args.get('restaurant_id')
     feeling = request.args.get('feeling')
     
     current_group[group_id]['Users'][user_id]['Feeling'][restaurant_id] = feeling
-
-    return ""
+    
+    # 全員一致だったらcurrent_groupに格納する
+    if all([ u['Feeling'][restaurant_id] for u in current_group[group_id]['Users'].values() ]):
+        current_group[group_id]['Unanimous'].append( restaurant_id )
+    
+    # 全会一致の店舗を知らせる
+    local_search_query = { 'uid': ','.join(current_group[group_id]['Unanimous']) }
+    return get_restaurant_info_from_local_search_query(current_group[group_id]['Coordinates'], local_search_query)
 
 
