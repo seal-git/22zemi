40bdcc598f644dea4a963771435943a0a26fac32
tokky08 <tokky.kz08@gmail.com>
2021-06-22 13:16:58 +0900

    ヤフーロコURLとカテゴリーの追加

--- python-flask/app/api_functions.py
+++ python-flask/app/api_functions.py
@@ -53,7 +53,7 @@ def get_restaurant_info_from_local_search_params(coordinates, local_search_param
         result_json[i]['Price'] = feature['Property']['Detail']['LunchPrice'] if lunch_or_dinner == 'lunch' and feature['Property']['Detail'].get('LunchFlag') == True else feature['Property']['Detail'].get('DinnerPrice')
         result_json[i]['TopRankItem'] = [feature['Property']['Detail']['TopRankItem'+str(j)] for j in range(MAX_LIST_COUNT) if 'TopRankItem'+str(j) in feature['Property']['Detail']] # TopRankItem1, TopRankItem2 ... のキーをリストに。
         result_json[i]['CassetteOwnerLogoImage'] = feature['Property']['Detail'].get('CassetteOwnerLogoImage')
-        result_json[i]['Category'] = feature['Category']
+        result_json[i]['Category'] = ",".join(feature['Category'][0].split(",")[:3])
         result_json[i]['UrlYahooLoco'] = "https://loco.yahoo.co.jp/place/" + result_json[i]['Restaurant_id']
 
         lead_image = [feature['Property']['LeadImage']] if 'LeadImage' in feature['Property'] else []
