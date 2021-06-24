3fdf9aaee0dd0d848e290864a5a2c64bef6fcfe4
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-06-24 21:10:53 +0900

    receive data from API

--- react/src/components/Alone.js
+++ react/src/components/Alone.js
@@ -1,46 +1,73 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Buttons from "./Buttons";
 import RestaurantInformation from "./RestaurantInformation";
+import axios from "axios";
 
 // ひとりで決める
 function Alone() {
-  // サンプルデータを使ったプロトタイピング
-  // 実際にはAPIからデータを得る
+  // APIからデータを得る
   const [idx, setIndex] = useState(0)
-  const samples = [
-    {
-      "name": "神鶏 市ヶ谷店",
-      "mapURL": "https://map.yahoo.co.jp/route/train?from=%E5%9B%9B%E3%83%84%E8%B0%B7%E9%A7%85&to=%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E5%B8%82%E8%B0%B7%E7%94%B0%E7%94%BA1-3-5&t=1&y=202106&d=20&h=20&m=44&sort=1&lat=35.68754&lon=139.73242&zoom=15&maptype=basic",
-      "reserveURL":"https://loco.yahoo.co.jp/place/g-8c8pQERfq2g/"
-    },
-    {
-      "name": "一軒家イタリアン ＥＬＳＡ 新宿本店",
-      "mapURL": "https://map.yahoo.co.jp/route/train?from=%E5%9B%9B%E3%83%84%E8%B0%B7%E9%A7%85&to=%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%96%B0%E5%AE%BF%E5%8C%BA%E6%96%B0%E5%AE%BF3-10-5&t=1&y=202106&d=20&h=20&m=44&sort=1&lat=35.68604&lon=139.71303&zoom=16&maptype=basic",
-      "reserveURL": "https://loco.yahoo.co.jp/place/g-1dIwmpfPtko"
-    },
-  ]
-  const [data, setData] = useState(samples[0])
+  const [isLoading, setIsLoading] = useState(true)
+  const [dataList, setDataList] = useState([{"Name":"Hello"}])
+  const [data, setData] = useState(dataList[0])
+
+  const getInfo = () => {
+    axios.post('/api/info',{ params: {
+      user_id:1,
+      group_id:1
+    }
+    })
+    .then(function(response){
+      console.log(response)
+      let dataList = response['data']
+      console.log(dataList[0])
+      setIndex(0)
+      setData(dataList[0])
+      setDataList(dataList)
+      setIsLoading(false)
+    })
+    .catch((error) => {
+      console.log("error:",error);
+    });
+  }
+
+  useEffect( ()=> {
+    if(isLoading) {
+      setIsLoading(false)
+      getInfo()
+    }
+  },[])
+
+  const turnCard = () => {
+    if(idx>=dataList.length) return
+    const nextIdx = idx + 1
+    if(nextIdx===dataList.length){
+      setIsLoading(true)
+      getInfo()
+    }else{
+      setIndex(nextIdx)
+      setData(dataList[nextIdx])
+    }
+  }
 
   // 各ボタンに対応する関数
   const reject = () => {
     console.log("reject")
-    const nextIdx = 1 - idx
-    setIndex(nextIdx)
-    setData(samples[nextIdx])
+    if(isLoading) return;
+    turnCard()
   }
   const keep = () => {
     console.log("keep")
-    const nextIdx = 1 - idx
-    setIndex(nextIdx)
-    setData(samples[nextIdx])
+    if(isLoading) return;
+    turnCard()
   }
   const direct = () => {
     console.log("direct")
-    window.open(data.mapURL)
+    window.open(data.UrlYahooMap)
   }
   const reserve = () => {
     console.log("reserve")
-    window.open(data.reserveURL);
+    window.open(data.UrlYahooLoco)
   }
   return (
     <div className="Alone">
