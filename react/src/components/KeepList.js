9f1b3104299c771ee6441253c5467cf676f6d805
KanekoKenichiro <fanta.remio.ken@gmail.com>
2021-06-27 23:17:53 +0900

    途中やで

--- react/src/components/KeepList.js
+++ react/src/components/KeepList.js
@@ -4,10 +4,31 @@ import axios from "axios";
 import sampleData from "./sampleData.json";
 
 import { makeStyles } from '@material-ui/core/styles';
+import { Box } from "@material-ui/core";
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
-
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    select: {
+        height: '40px',
+        color: 'black',
+        background:
+            'linear-gradient(116.73deg, #FFCD4E 27.25%, #FFB74A 71.71%)',
+        border: '0px',
+        borderRadius: '10px'
+    }
+}));
 
 function KeepList(props) {
+
+    const classes = useStyles();
 
     const sample = sampleData;
 
@@ -15,12 +36,35 @@ function KeepList(props) {
 
     return (
         <div>
-            <h1>キープリスト</h1>
+            <Box>
+                <FormControl variant="outlined" className={classes.formControl}>
+                    <Select
+                        native
+                        value="osusu"
+                        inputProps={{
+                            name: 'age',
+                            id: 'outlined-age-native-simple',
+                        }}
+                        className={classes.select}
+                        onChange={selectControll}
+                        id="select"
+                    >
+                        <option value={10} >おすすめ順</option>
+                        <option value={20}>評価が高い順</option>
+                        <option value={30}>距離が近い順</option>
+                    </Select>
+                </FormControl>
+            </Box>
             {sample.map((data) => (
                 <KeepListTile data={data} />
             ))}
         </div>
     );
+}
+
+function selectControll() {
+    console.log("a");
+
 }
 
 export default KeepList;
--- react/src/components/KeepListTile.js
+++ react/src/components/KeepListTile.js
@@ -1,91 +1,117 @@
-import { CardContent } from '@material-ui/core';
+import { Box, CardContent } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card'
+import { createMuiTheme } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import IconButton from '@material-ui/core/IconButton';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Typography from '@material-ui/core/Typography';
 
+import CloseIcon from '@material-ui/icons/Close';
+import RoomIcon from '@material-ui/icons/Room';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
 const useStyles = makeStyles((theme) => ({
-    tile: {
-        margin: '10px'
-    },
     root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
+        margin: '0 0 10px 0',
+        padding: '0px',
     },
     gridList: {
         flexWrap: 'nowrap',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
         transform: 'translateZ(0)',
-    },
-    title: {
-        color: theme.palette.primary.light,
+        margin: '20px',
+        padding: '5px'
     },
     titleBar: {
         background:
             'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
     },
+    cardContent: {
+        padding: '5px 10px'
+    },
+    cardContentSub: {
+        display: 'flex',
+        margin: '0px',
+        padding: '0px 0px',
+    },
+    cardActions: {
+        padding: '0',
+    },
+    textShopName: {
+        fontSize: '1.2rem',
+        fontWeight: 'bold'
+    },
+    textSecondary: {
+        fontSize: '0.8rem'
+    },
+    textVotes: {
+        margin: '5px 0 0 30px',
+        padding: '5px 0 0 0',
+        textAlign: 'center',
+        fontSize: '1.2rem',
+        width: '100%',
+        backgroundColor: '#FFECC8',
+        borderTopLeftRadius: '24px',
+        border: 'solid 1px #A03A00',
+        color: '#777'
+    },
+    textVoteResult: {
+        fontSize: '1.4rem'
+    }
 }));
-
-
 
 function KeepListTile(props) {
     const classes = useStyles();
     const space = <span className={classes.space}>　</span>;
 
     return (
-        <Card variant="outlined" className={classes.tile}>
-            {/* <GridList cellHeight={160} rows={1}>
-                {props.data.Images.map((tile) => (
-                    <GridListTile key={tile}>
-                        <img src={tile} className={classes.tileImg} />
-                    </GridListTile>
-                ))}
-            </GridList> */}
+        <Card variant="outlined" className={classes.root}>
 
-            <GridList className={classes.gridList} cols={2.5}>
+            <GridList className={classes.gridList} cols={2.5} cellHeight={100}>
                 {props.data.Images.map((tile) => (
-                    <GridListTile key={tile}>
+                    <GridListTile key={tile} className={classes.gridListTile}>
                         <img src={tile} />
-                        <GridListTileBar
-                            // title={tile.title}
-                            classes={{
-                                root: classes.titleBar,
-                                title: classes.title,
-                            }}
-                        // actionIcon={
-                        //     <IconButton aria-label={`star ${tile.title}`}>
-                        //         <StarBorderIcon className={classes.title} />
-                        //     </IconButton>
-                        // }
-                        />
                     </GridListTile>
                 ))}
             </GridList>
             <CardContent className={classes.cardContent}>
 
-                <Typography component="h2">
+                <Typography className={classes.textShopName}>
                     {props.data.Name}
                 </Typography>
-                <Typography className={classes.title} color="primary" component="h4" gutterBottom>
-                    ★★★☆☆３
+                <Typography variant="h6" className={classes.textSecondary} color="primary" >
+                    ★★★☆☆{space}3
                 </Typography>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                <Typography className={classes.textSecondary}>
                     {props.data.Category}{space}~{props.data.Price}円
                 </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    {props.data.Distance}m
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {props.data.CatchCopy}
+                <Typography className={classes.textSecondary} >
+                    300{space}10:00~22:00{space}営業中
                 </Typography>
             </CardContent>
-        </Card>
+            <Box className={classes.cardContentSub}>
+                <CardActions className={classes.cardActions}>
+                    <IconButton>
+                        <CloseIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RoomIcon />
+                    </IconButton>
+                    <IconButton>
+                        <InfoOutlinedIcon />
+                    </IconButton>
+                </CardActions>
+                <Typography className={classes.textVotes}>
+                    <FavoriteIcon style={{ fontSize: 18 }} />
+                    <span className={classes.textVoteResult}>12</span>
+                    /20
+                </Typography>
+            </Box>
+        </Card >
     );
 }
 
