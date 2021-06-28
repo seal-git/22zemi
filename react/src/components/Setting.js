439a787db4180b5cfb5180484d5cf60c49866c55
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-06-29 02:25:18 +0900

    add elements

--- react/src/components/Setting.js
+++ react/src/components/Setting.js
@@ -3,8 +3,8 @@ import { Button, Grid,FormControl,Input, Container,TextField, Typography } from 
 import { makeStyles } from "@material-ui/core";
 import SearchButtonOne from "./search_button_one.png"
 import SearchButtonAll from "./search_button_all.png"
-// 設定画面
 
+// 現在時刻を文字列で取得
 const getCurrentTime = () => {
   const date = new Date()
   const time = ('00' + date.getHours().toString()).slice(-2) + ':' + ('00'+date.getMinutes().toString()).slice(-2)
@@ -12,60 +12,69 @@ const getCurrentTime = () => {
   return time
 }
 
+// 設定画面
 function Setting(props) {
+
+  // 「決める」ボタンが叩かれたときの処理
   const proceedToSelection = (newMode,groupId) => {
+    // モード切り替え
+    // 暫定的な処置。ポップアップなどで manage したい
     if(newMode!==props.mode){
       props.turnMode(groupId)
     }
+
+    // 検索条件を取得
+    const area = document.getElementById("inputArea").value
+    const genre = document.getElementById("inputGenre").value
+    const time = +document.getElementById("inputTime").value.slice(0,2) // time は int にする
+    console.log(area,genre,time)
+
+    // パラメータを更新
+    // props.setParams({"area":area, "genre":genre, "time":time })
+
+    // Selection に移る
     props.setView("Selection")
   }
-  const CustomTextField = (props) => {
+  const CustomInput = (props) => {
     return(
-      <Input defaultValue={props.defaultValue} inputProps={{ 'aria-label': 'description' }} />
+      <Input id={props.id} defaultValue={props.defaultValue} inputProps={{ 'aria-label': 'description' }} />
     )
-  }
-  const TimePicker = () => {
-    return (
-      <form className='container' noValidate>
-        <TextField
-          id="time-local"
-          type="time"
-          defaultValue={getCurrentTime()}
-          inputProps={{ min: 0, style: { textAlign: 'left' } }}
-          InputLabelProps={{
-            shrink: false,
-          }}
-        />
-      </form>
-    );
   }
   return (
     <div className="setting">
-      <h1>飯T</h1>
-      <Grid container spacing={2}>
-        <Grid item xs='4' className='label-area'>
-          <Typography>エリア</Typography>
-        </Grid>
-        <Grid item xs='7'>
-          <CustomTextField defaultValue="四ツ谷駅"></CustomTextField>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs='4' className='label-genre'>
-          ジャンル
-        </Grid>
-        <Grid item xs='7'>
-          <CustomTextField defaultValue="居酒屋"></CustomTextField>
-        </Grid>
-      </Grid>
-      <Grid container spacing={2}>
-        <Grid item xs='4' className='label-time'>
-          時間
-        </Grid>
-        <Grid item xs='7'>
-          <TimePicker />
-        </Grid>
-      </Grid>
+      <div class="box-title">
+        <div class="label-title">
+          <Typography>Reskima</Typography>
+        </div>
+      </div>
+      <div class="box-area">
+        <label className="label-area">
+          <Typography>
+            エリア
+          </Typography>
+        </label>
+      </div>
+      <div class="input-area">
+        <CustomInput defaultValue="四ツ谷駅" id="inputArea"></CustomInput>
+      </div>
+      <div class="box-genre">
+        <label className="label-genre">
+          <Typography>
+            ジャンル
+          </Typography>
+        </label>
+      </div>
+      <div class="input-genre">
+        <CustomInput defaultValue="居酒屋" id="inputGenre"></CustomInput>
+      </div>
+      <div class="box-time">
+        <label className="label-time">
+          <Typography>
+            時間
+          </Typography>
+        </label>
+      </div>
+      <TextField className="input-time" defaultValue={getCurrentTime()} type="time" id="inputTime" />
       <button className="button-alone" onClick={()=>{proceedToSelection("Alone","")}}>
         <img 
           src={SearchButtonOne}
