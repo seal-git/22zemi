04168042756fbc9a8379f4091634574165f0879c
yuki yoshinari <seal0828.4477@gmail.com>
2021-06-30 00:01:35 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # GCEでdocker-composeコマンドが使えなかったため、それ用のdocker imageを使用
 cd 22zemi
+git checkout production
 git pull origin production:production
 #docker run \
 #--rm -v /var/run/docker.sock:/var/run/docker.sock \
