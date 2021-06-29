42e3d1fe8fb422d60af1d7d75a58553a2a20a8a1
yuki yoshinari <seal0828.4477@gmail.com>
2021-06-30 00:17:59 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 cd 22zemi
 git checkout production
 git pull origin production:production
-#docker run \
-#--rm -v /var/run/docker.sock:/var/run/docker.sock \
-#-v "$PWD:/$PWD" -w="/$PWD" \
-#docker/compose:1.29.2 \
-#up -d
+docker run \
+--rm -v /var/run/docker.sock:/var/run/docker.sock \
+-v "$PWD:/$PWD" -w="/$PWD" \
+docker/compose:1.29.2 \
+up -d
