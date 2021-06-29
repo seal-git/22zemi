5df5da6a1cd6294c747ea9d2f0126d629a5c7f18
yuki yoshinari <seal0828.4477@gmail.com>
2021-06-29 13:29:13 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 # GCEでdocker-composeコマンドが使えなかったため、それ用のdocker imageを使用
 cd 22zemi
 git pull origin production:production
-docker run \
---rm -v /var/run/docker.sock:/var/run/docker.sock \
--v "$PWD:/$PWD" -w="/$PWD" \
-docker/compose:1.29.2 \
-up -d
+#docker run \
+#--rm -v /var/run/docker.sock:/var/run/docker.sock \
+#-v "$PWD:/$PWD" -w="/$PWD" \
+#docker/compose:1.29.2 \
+#up -d
