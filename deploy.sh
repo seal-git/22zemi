ba48e5f3f76a37a881227286e65a351ee50f7718
yuki yoshinari <seal0828.4477@gmail.com>
2021-06-30 06:18:34 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,14 @@ cd 22zemi
 git checkout production
 git fetch origin
 git merge origin/production
+docker stop react python-flask mysql
+
+docker run \
+--rm -v /var/run/docker.sock:/var/run/docker.sock \
+-v "$PWD:/$PWD" -w="/$PWD" \
+docker/compose:1.29.2 \
+build
+
 docker run \
 --rm -v /var/run/docker.sock:/var/run/docker.sock \
 -v "$PWD:/$PWD" -w="/$PWD" \
