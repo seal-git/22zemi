148043d41732ceb9fe9fc6e573cf2c1dafa62649
yuki yoshinari <seal0828.4477@gmail.com>
2021-07-19 21:04:20 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GCEでdocker-composeコマンドが使えなかったため,それ用のdocker imageを使用
-docker stop react python-flask mysql
+docker stop react python-flask mysql https
 cd 22zemi
 git checkout production
 git fetch origin
