58181a9cc43c7aa72c07b36346f5882fd236c76b
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2021-08-13 00:50:12 +0900

    replace react-tinder-card with react-spring

--- react/src/components/Selection.js
+++ react/src/components/Selection.js
@@ -3,13 +3,12 @@ import "./Selection.css"
 // パッケージからインポート
 import axios from "axios"
 import { useEffect, useState } from "react"
-import TinderCard from 'react-tinder-card'
 // 他のファイルからインポート
 import { assignNumGlobal } from './global'
 import ButtonToChangeMode from "./ButtonToChangeMode"
 import ButtonToInvite from "./ButtonToInvite"
 import Credit from "./Credit"
-import RestaurantInformation from "./RestaurantInformation"
+import RestaurantInformationDeck from './RestaurantInformationDeck'
 import noImageIcon from "..//img/no_image.png"
 
 const initDataList = [{
@@ -22,21 +21,17 @@ const emptyDataList = [{
   "Images": [noImageIcon, noImageIcon]
 }]
 
-var wrapperStyle = {
-  margin: 0,
-}
-
 /*
  スワイプでお店を選ぶコンポーネント
  */
 function Selection(props) {
   const [dataList, setDataList] = useState(initDataList)
-  let cardNum = dataList.length
   let isLoading = false
 
   // APIからお店のデータを得る
   const getInfo = (newUserId, newGroupId) => {
     if (isLoading) return;
+    if(dataList!==initDataList) setDataList(initDataList)
     isLoading = true
 
     // ユーザID を設定
@@ -62,7 +57,7 @@ function Selection(props) {
         console.log(response)
         let dataList = response['data']
         // let dataList = sampledata
-        cardNum = dataList.length
+        let cardNum = dataList.length
         if (cardNum > 0) {
           console.log(dataList[0])
           setDataList(dataList)
@@ -82,20 +77,6 @@ function Selection(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  // カードをめくる
-  const turnCard = () => {
-    // データリストが初期状態の場合何もしない
-    if (dataList[0].Name === initDataList[0].Name) return;
-    // カード枚数を1減らす
-    cardNum -= 1
-    if (cardNum === 0) {
-      // データリストの取得待ちであることを明示する
-      cardNum = 1
-      setDataList(initDataList)
-      // リスト取得
-      getInfo()
-    }
-  }
 
   // APIにキープ・リジェクトを送信する
   const sendFeeling = (feeling, restaurant_id) => {
@@ -109,7 +90,6 @@ function Selection(props) {
       .then(function (response) {
         console.log(response)
         assignNumGlobal(response.data)
-        turnCard()
       })
       .catch((error) => {
         console.log("error:", error);
@@ -131,63 +111,13 @@ function Selection(props) {
   // ButtonToChangeMode 用に加工
   const turnMode = () => {
     // データリストの取得待ちであることを明示する
-    cardNum = 1
     setDataList(initDataList)
     // モード切り替え
     props.turnMode()
   }
 
-  // カードの高さを指定する
-  function getAdaptiveStyle() {
-    // let height = window.innerHeight
-    let height = document.getElementById("selection").getBoundingClientRect().height
-    let wrapperStyle = {
-      height: height,
-    }
-    return wrapperStyle
-  };
-  //windowサイズの変更検知のイベントハンドラを設定
-  window.addEventListener('load', () => {
-    wrapperStyle = getAdaptiveStyle()
-  })
-
-  // swipe 操作をハンドル
-  const handleLeftScreen = (dir, restaurant_id) => {
-    console.log(dir)
-    if (dir === 'right') {
-      keep(restaurant_id)
-    } else {
-      reject(restaurant_id)
-    }
-  }
-  const CardsContainer = (props) => {
-    // スワイプできない方向を設定
-    let prevents = ['up', 'down']
-    if (props.dataList === initDataList || props.dataList === emptyDataList) {
-      prevents = ['up', 'down', 'right', 'left']
-    }
-    // お店ごとに情報カードを生成
-    // TinderCard として扱うことでスワイプを可能にしている
-    return (props.dataList.reverse().map((data) => {
-      return (
-        <TinderCard
-          onCardLeftScreen={(dir) => {
-            handleLeftScreen(dir, data.Restaurant_id);
-          }}
-          preventSwipe={prevents}
-          className={'tinder-ui'}
-        >
-          <RestaurantInformation data={data}
-            wrapperStyle={wrapperStyle} />
-        </TinderCard>
-      )
-    }))
-  }
-  var display_style;
-  props.mode === "Alone" ? display_style = { display: "none" } : display_style = null;
-
-  return (
-    <div className="Selection-wrapper">
+  let renderButtonToChangeMode = () =>{
+    return (
       <ButtonToChangeMode
         mode={props.mode}
         turnMode={turnMode}
@@ -196,25 +126,55 @@ function Selection(props) {
         produceId={props.produceId}
         getInfo={getInfo}
       />
+    )
+  }
+
+  let renderButtonToInvite = () =>{
+    return (
+        <ButtonToInvite
+          url={props.inviteUrl}
+          groupId={props.groupId} 
+          callInviteUrl={props.callInviteUrl}
+          />
+    )
+  }
+
+  let renderRestaurantInformationDeck = () =>{
+    return (
+      <RestaurantInformationDeck
+        dataList={dataList}
+        hasRestaurant={
+          dataList !== initDataList 
+          && dataList !== emptyDataList}
+        keep={keep}
+        reject={reject}
+        getInfo={getInfo}
+      />
+    )
+  }
+
+  var display_style;
+  props.mode === "Alone" 
+  ? display_style = { display: "none" } 
+  : display_style = null;
+
+  return (
+    <div className="Selection-wrapper">
+      { renderButtonToChangeMode() }
       <div className="Selection-header">
         <div className={"Selection-header-content"}
           style={display_style}>
-          <ButtonToInvite
-            url={props.inviteUrl}
-            groupId={props.groupId} 
-            callInviteUrl={props.callInviteUrl}
-            />
+          { renderButtonToInvite() }
           <div className="group-id">
             ルームID:{props.groupId}
           </div>
         </div>
       </div>
       <div className="Selection" id={"selection"}>
-        {/* <RestaurantInformation data={dataList[idx]} wrapperStyle={wrapperStyle} /> */}
-        <div className='card-container'>
-          <CardsContainer dataList={dataList} />
+        <div className='information-deck'>
+          {/* <RestaurantInformation data={dataList[idx]} wrapperStyle={wrapperStyle} /> */}
+          { renderRestaurantInformationDeck() }
         </div>
-        {/* <Buttons reject={reject} keep={keep} /> */}{/*ボタンを取り付けようとすると工数が激増する。一旦保留*/}
       </div>
       <Credit />
     </div>
