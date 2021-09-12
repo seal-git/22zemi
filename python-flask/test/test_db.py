92a824594b1ddd510ebd14f3c56aae353e0e3b48
seal-git <seal0828.4477@gmail.com>
2021-09-13 01:41:19 +0900

    update

--- python-flask/test/test_db.py
+++ python-flask/test/test_db.py
@@ -1,4 +1,3 @@
-from app.models import get_sample_db
 from app import app_
 from flask import jsonify
 
@@ -7,7 +6,7 @@ pytestはpython-flaskのルートディレクトリ(app.pyのあるところ)で
 """
 
 
-def test_get_sample_db():
-    with app_.app_context():
-        result = get_sample_db()
-        assert(b"content" in result.data)
+# def test_get_sample_db():
+#     with app_.app_context():
+#         result = get_sample_db()
+#         assert(b"content" in result.data)
