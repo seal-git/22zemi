769e49b5af2311b36015e85de1ca98959e377c68
nakamuratoru <toru.n.2567@gmail.com>
2021-09-26 03:41:44 +0900

    距離の表示を追加

--- python-flask/app/models.py
+++ python-flask/app/models.py
@@ -46,12 +46,12 @@ def create_response_from_restaurants_info(group_id, user_id, restaurants_info):
 
     # レスポンスするためにいらないキーを削除する
     restaurants_info = [r.get_dict_for_react() for r in restaurants_info]
-    response_keys = ['Restaurant_id', 'Name', 'Address', 'CatchCopy', 'Price',
+    response_keys = ['Restaurant_id', 'Name', 'Distance', 'Address', 'CatchCopy', 'Price',
                      'Category', 'UrlWeb', 'UrlMap', 'ReviewRating',
                      'BusinessHour', 'Genre', 'Images', 'ImagesBinary']
     response = [{k: v for k, v in r.items() if k in response_keys} for r in
                 restaurants_info]  # response_keysに含まれているキーを残す
-    pprint.PrettyPrinter(indent=2).pprint(response)
+    #pprint.PrettyPrinter(indent=2).pprint(response)
 
     return json.dumps(response, ensure_ascii=False)
 
