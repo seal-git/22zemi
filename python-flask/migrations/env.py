f590076ec337015f42176dfe65bab9c09011b75e
yuki yoshinari <seal0828.4477@gmail.com>
2021-11-01 14:42:10 +0900

    add automigration

--- python-flask/migrations/env.py
+++ python-flask/migrations/env.py
@@ -64,6 +64,14 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
+
+    def process_revision_directives(context, revision, directives):
+        if config.cmd_opts.autogenerate:
+            script = directives[0]
+            if script.upgrade_ops.is_empty():
+                directives[:] = []
+
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
@@ -72,7 +80,9 @@ def run_migrations_online():
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            process_revision_directives=process_revision_directives,
         )
 
         with context.begin_transaction():
