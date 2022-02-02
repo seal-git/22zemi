473091bc476eb737b5b4d50700c0d5c4e7ed1587
seal-git <seal0828.4477@gmail.com>
2022-02-02 14:49:07 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GCEでdocker-composeコマンドが使えなかったため,それ用のdocker imageを使用
-docker stop react python-flask mysql https
+docker stop react python-flask mysql
 cd 22zemi
 git checkout production
 git fetch origin
