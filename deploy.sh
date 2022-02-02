161c874eae70536f0c5bde3d8460fe9f29a3731d
seal-git <seal0828.4477@gmail.com>
2022-02-02 14:47:39 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,5 @@ git checkout production
 git fetch origin
 git reset --hard origin/production
 
-docker run \
---rm -v /var/run/docker.sock:/var/run/docker.sock \
--v "$PWD:/$PWD" -w="/$PWD" \
-docker/compose:1.29.2 \
-build
-
-docker run \
---rm -v /var/run/docker.sock:/var/run/docker.sock \
--v "$PWD:/$PWD" -w="/$PWD" \
-docker/compose:1.29.2 \
-up -d
+docker-compose build
+docker-compose up
