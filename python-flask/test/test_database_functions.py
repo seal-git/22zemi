a90ea1249c37672d1d682375eaf1c1f66e24b65b
milmilk <o1610158@edu.cc.uec.ac.jp>
2022-06-09 23:45:06 +0900

    fix test_load_stable_restaurants_info()

--- python-flask/test/test_database_functions.py
+++ python-flask/test/test_database_functions.py
@@ -3,13 +3,14 @@ from app.internal_info import *
 import pprint
 
 def test_load_stable_restaurants_info():
-    r_ids = ["5ce1ffb50b7c586e52e37235d076fd7ba6e647d4"]
+    group_id = 456789
     session = database_functions.get_db_session()
+    r_ids = session.query(Restaurant.id).first()
     fetch_restaurants = session.query(Restaurant).all()
     print(fetch_restaurants)
     for f in fetch_restaurants:
         print(f.id)
-    r_info = load_restaurants_info(r_ids)
+    r_info = load_restaurants_info(r_ids, group_id)
 
     [pprint.PrettyPrinter(indent=2).pprint(r.name) for r in r_info]
     assert len(r_info)==1
